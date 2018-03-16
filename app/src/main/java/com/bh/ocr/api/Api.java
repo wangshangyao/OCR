53503878b1718ad750344f941cb9900452c644d4
wsy <1767112006@qq.com>
2018-03-16 10:31:21 +0800

    增加上传图片接口

--- app/src/main/java/com/bh/ocr/api/Api.java
+++ app/src/main/java/com/bh/ocr/api/Api.java
@@ -1,5 +1,5 @@
 package com.bh.ocr.api;
 
 public class Api {
-    public static String getCard = "http://rz.kfsbj.cn:8081/face/checkPerson.action";
+    public static String getCard = "http://192.168.1.206:8080/KFwebService/face/getCard.action";
 }
